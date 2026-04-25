95b8c665b4b4530a9891c7d89247ddea64db0da4
nico caputto <guawowultimo@gmail.com>
2026-04-25 04:25:49 -0300

    funciona notificacion cuandoo quiere

---
+++ PracticoMobileApp/Platforms/Android/FirebaseMessagingService.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content;
+using Firebase.Messaging;
+
+namespace PracticoMobileApp.Platforms.Android
+{
+    [Service(Exported = false)]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class FirebaseMessagingService : Firebase.Messaging.FirebaseMessagingService
+    {
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+        }
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            base.OnMessageReceived(message);
+
+            if (message?.GetNotification() != null)
+            {
+                SendNotificationToAndroid(
+                    message.GetNotification().Title,
+                    message.GetNotification().Body
+                );
+            }
+        }
+
+        private void SendNotificationToAndroid(string title, string body)
+        {
+            NotificationManager manager = (NotificationManager)GetSystemService(NotificationService);
+
+            Notification.Builder builder = new Notification.Builder(this, "default_channel_id")
+                .SetSmallIcon(this.ApplicationInfo.Icon)
+                .SetContentTitle(title)
+                .SetContentText(body)
+                .SetAutoCancel(true);
+
+            manager?.Notify(1, builder.Build());
+        }
+    }
+}
